aa098f34f2b772a4cebea4c93b9f453c7b24140f
SyMind <dacongsama@live.com>
2023-04-07 19:53:21 +0800

    feat: svg key & value to jsx

--- src/hast_to_swc_ast.rs
+++ src/hast_to_swc_ast.rs
@@ -1,6 +1,83 @@
+use swc::atoms::JsWord;
 use swc_common::DUMMY_SP;
 use swc_ecmascript::ast::*;
-use swc_xml::{visit::{Visit, VisitWith}, ast::{Document}};
+use swc_xml::{visit::{Visit, VisitWith}};
+use regex::{Regex, Captures};
+
+fn kebab_case(str: &str) -> String {
+    let kebab_regex = Regex::new(r"[A-Z\u00C0-\u00D6\u00D8-\u00DE]").unwrap();
+    kebab_regex.replace_all(str, |caps: &Captures| format!("-{}", &caps[0].to_lowercase())).into()
+}
+
+fn convert_aria_attribute(kebab_key: &str) -> String {
+    let parts: Vec<&str> = kebab_key.split('-').collect();
+    let aria = parts[0];
+    let lowercase_parts: String = parts[1..].join("").to_lowercase();
+    format!("{}-{}", aria, lowercase_parts)
+}
+
+fn get_key(attr_name: &str, tag_name: &str) -> Ident {
+    let lower_case_name = attr_name.to_lowercase();
+    let rc_key = {
+        match tag_name {
+            "input" => {
+                match lower_case_name.as_str() {
+                    "checked" => Some("defaultChecked"),
+                    "value" => Some("defaultValue"),
+                    "maxlength" => Some("maxLength"),
+                    _ => None,
+                }
+            },
+            "form" => {
+                match lower_case_name.as_str() {
+                    "enctype" => Some("encType"),
+                    _ => None,
+                }
+            },
+            _ => None,
+        }
+    };
+
+    if let Some(k) = rc_key {
+        return Ident {
+            span: DUMMY_SP,
+            sym: k.into(),
+            optional: false,
+        }
+    }
+
+    let kebab_key = kebab_case(&attr_name);
+
+    if kebab_key.starts_with("aria-") {
+        return Ident {
+            span: DUMMY_SP,
+            sym: convert_aria_attribute(attr_name).into(),
+            optional: false,
+        }
+    }
+
+    if kebab_key.starts_with("data-") {
+        return Ident {
+            span: DUMMY_SP,
+            sym: attr_name.clone().into(),
+            optional: false,
+        }
+    }
+
+    Ident {
+        span: DUMMY_SP,
+        sym: attr_name.clone().into(),
+        optional: false,
+    }
+}
+
+fn get_value(attr_name: &str, value: &JsWord) -> JSXAttrValue {
+    return JSXAttrValue::Lit(Lit::Str(Str {
+        span: DUMMY_SP,
+        value: value.clone(),
+        raw: None
+    }))
+}
 
 pub struct HastVisitor {
     jsx: Option::<JSXElement>,
@@ -14,12 +91,25 @@ impl HastVisitor {
 
 impl Visit for HastVisitor {
     fn visit_element(&mut self, n: &swc_xml::ast::Element) {
-        println!("{:?}", n.attributes);
+        let attrs = n.attributes.iter().map(
+            |attr| {
+                let value = match attr.value.clone() {
+                    Some(v) => Some(get_value(&attr.name, &v)),
+                    None => None,
+                };
+
+                JSXAttrOrSpread::JSXAttr(JSXAttr {
+                    span: DUMMY_SP,
+                    name: JSXAttrName::Ident(get_key(&attr.name, &n.tag_name)),
+                    value,
+                })
+            }
+        ).collect::<Vec<JSXAttrOrSpread>>();
 
         let opening = JSXOpeningElement {
             span: DUMMY_SP,
             name: JSXElementName::Ident(Ident::new(n.tag_name.clone(), DUMMY_SP)),
-            attrs: vec![],
+            attrs,
             self_closing: true,
             type_args: None,
         };
@@ -35,7 +125,7 @@ impl Visit for HastVisitor {
     }
 }
 
-pub fn to_swc_ast(hast: Document) -> Option<JSXElement> {
+pub fn to_swc_ast(hast: swc_xml::ast::Document) -> Option<JSXElement> {
     let mut v = HastVisitor {
         jsx: None,
     };
@@ -102,7 +192,6 @@ mod tests {
     fn transforms_aria_x() {
         let code = "<svg aria-hidden=\"true\"></svg>";
         let result = transform(code);
-        println!("{}", result);
-        assert!(result == "<svg aria-hidden=\"true\" />;")
+        assert_eq!(result, "<svg aria-hidden=\"true\"/>;\n")
     }
 }
