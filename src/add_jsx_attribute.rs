5ea50fa3cdba37f7f8b52a03b7375d6f3da33b95
SyMind <dacongsama@live.com>
2023-04-16 22:55:23 +0800

    chore: unit test for add jsx attribute

--- src/add_jsx_attribute.rs
+++ src/add_jsx_attribute.rs
@@ -17,6 +17,7 @@ pub enum AttributePosition {
     End,
 }
 
+#[derive(Default)]
 pub struct Attribute {
     pub name: String,
     pub value: Option<AttributeValue>,
@@ -25,16 +26,21 @@ pub struct Attribute {
     pub position: Option<AttributePosition>,
 }
 
+pub struct Options {
+    elements: Vec<String>,
+    attributes: Vec<Attribute>,
+}
+
 pub struct Visitor {
     elements: Vec<String>,
     attributes: Vec<Attribute>,
 }
 
 impl Visitor {
-    pub fn new(elements: Vec<String>, attributes: Vec<Attribute>) -> Self {
+    pub fn new(opts: Options) -> Self {
         Self {
-            elements,
-            attributes,
+            elements: opts.elements,
+            attributes: opts.attributes,
         }
     }
 }
@@ -60,7 +66,7 @@ impl VisitMut for Visitor {
 
             let position = match position {
                 Some(position) => position,
-                None => &AttributePosition::Start,
+                None => &AttributePosition::End,
             };
 
             let new_attr = get_attr(*spread, &name, value.as_ref(), *literal);
@@ -154,24 +160,228 @@ fn get_attr_value(literal: bool, attr_value: Option<&AttributeValue>) -> Option<
                 },
                 AttributeValue::Str(value) => {
                     if literal {
+                        Some(JSXAttrValue::JSXExprContainer(JSXExprContainer {
+                            span: DUMMY_SP,
+                            expr: JSXExpr::Expr(Box::new(Expr::Ident(Ident {
+                                sym: value.to_string().into(),
+                                span: DUMMY_SP,
+                                optional: false,
+                            }))),
+                        }))
+                    } else {
                         Some(JSXAttrValue::Lit(Lit::Str(Str {
                             span: DUMMY_SP,
                             value: value.to_string().into(),
                             raw: None,
                         })))
-                    } else {
-                        Some(JSXAttrValue::JSXExprContainer(JSXExprContainer {
-                            span: DUMMY_SP,
-                            expr: JSXExpr::Expr(Box::new(Expr::Lit(Lit::Str(Str {
-                                span: DUMMY_SP,
-                                value: value.to_string().into(),
-                                raw: None,
-                            })))),
-                        }))
                     }
                 },
             }
         },
         None => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use swc_core::{
+        common::{SourceMap, FileName},
+        ecma::{
+            ast::*,
+            parser::{lexer::Lexer, Parser, StringInput, Syntax, EsConfig},
+            codegen::{text_writer::JsWriter, Emitter, Config},
+            visit::{FoldWith, as_folder}
+        },
+    };
+
+    use super::*;
+
+    fn code_test(input: &str, opts: Options, expected: &str) {
+        let cm = Arc::<SourceMap>::default();
+        let fm = cm.new_source_file(FileName::Anon, input.to_string());
+
+        let lexer = Lexer::new(
+            Syntax::Es(EsConfig {
+                decorators: true,
+                jsx: true,
+                ..Default::default()
+            }),
+            EsVersion::EsNext,
+            StringInput::from(&*fm),
+            None,
+        );
+
+        let mut parser = Parser::new_from(lexer);
+        let module = parser.parse_module().unwrap();
+
+        let module = module.fold_with(&mut as_folder(Visitor::new(opts)));
+
+        let mut buf = vec![];
+        let mut emitter = Emitter {
+            cfg: Config {
+                ..Default::default()
+            },
+            cm: cm.clone(),
+            comments: None,
+            wr: JsWriter::new(cm, "", &mut buf, None),
+        };
+        emitter.emit_module(&module).unwrap();
+        let result = String::from_utf8_lossy(&buf).to_string();
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn should_add_simple_attribute() {
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "disabled".to_string(),
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div disabled/>;"#,
+        );
+    }
+
+    #[test]
+    fn should_add_attribute_with_value() {
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "disabled".to_string(),
+                        value: Some(AttributeValue::Bool(true)),
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div disabled={true}/>;"#,
+        );
+
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "disabled".to_string(),
+                        value: Some(AttributeValue::Str("true".to_string())),
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div disabled="true"/>;"#,
+        );
+
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "disabled".to_string(),
+                        value: Some(AttributeValue::Num(200.0)),
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div disabled={200}/>;"#,
+        );
+    }
+
+    #[test]
+    fn should_add_literal_attribute() {
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "ref".to_string(),
+                        value: Some(AttributeValue::Str("ref".to_string())),
+                        literal: true,
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div ref={ref}/>;"#,
+        );
+
+        code_test(
+            r#"<div/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "ref".to_string(),
+                        value: Some(AttributeValue::Str("ref ? ref : null".to_string())),
+                        literal: true,
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div ref={ref ? ref : null}/>;"#,
+        );
+    }
+
+    #[test]
+    fn should_add_spread_attribute() {
+        code_test(
+            r#"<div foo><span/></div>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "props".to_string(),
+                        position: Some(AttributePosition::Start),
+                        spread: true,
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div {...props} foo><span/></div>;"#,
+        );
+
+        code_test(
+            r#"<div><span foo="bar"/></div>;"#,
+            Options {
+                elements: vec!["span".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "props".to_string(),
+                        position: Some(AttributePosition::End),
+                        spread: true,
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div><span foo="bar" {...props}/></div>;"#,
+        );
+    }
+
+    #[test]
+    fn should_replace_attribute() {
+        code_test(
+            r#"<div disabled/>;"#,
+            Options {
+                elements: vec!["div".to_string()],
+                attributes: vec![
+                    Attribute {
+                        name: "disabled".to_string(),
+                        value: Some(AttributeValue::Bool(false)),
+                        ..Attribute::default()
+                    }
+                ],
+            },
+            r#"<div disabled={false}/>;"#,
+        );
     }
 }
