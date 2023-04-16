6cc99c360143f5b07561f13f67a280f85908bbcd
SyMind <dacongsama@live.com>
2023-04-16 16:01:44 +0800

    0.0.1

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,6 +1,8 @@
+import test from 'ava'
 import { transform } from '../index.js'
 
-const svgCode = `
+test('sum from native', async t => {
+  const svgCode = `
 <svg xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink">
   <rect x="10" y="10" height="100" width="100"
@@ -8,9 +10,19 @@ const svgCode = `
 </svg>
 `
 
-const jsCode = await transform(
-  svgCode,
-  { icon: true },
-  { componentName: 'MyComponent' },
-)
-console.log(jsCode)
+  const jsCode = await transform(
+    svgCode,
+    { icon: true },
+    { componentName: 'MyComponent' },
+  )
+
+  const expected = `import * as React from "react";
+const MyComponent = (props)=><svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="1em" height="1em"><rect x={10} y={10} height={100} width={100} style={{
+        stroke: "#ff0000",
+        fill: "#0000ff"
+    }}/></svg>;
+export default MyComponent;
+`
+
+  t.is(jsCode, expected)
+})
