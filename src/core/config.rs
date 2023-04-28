023a97246d379a9c526935749d8ecc950bef9934
SyMind <dacongsama@live.com>
2023-04-28 15:50:01 +0800

    feat: desrialize expand props

--- src/core/config.rs
+++ src/core/config.rs
@@ -15,11 +15,54 @@ impl Default for Icon {
     }
 }
 
+// Untagged enums with empty variants (de)serialize in unintuitive ways
+// here: https://github.com/serde-rs/serde/issues/1560
+macro_rules! named_unit_variant {
+    ($variant:ident) => {
+        pub mod $variant {
+            pub fn serialize<S>(serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                serializer.serialize_str(stringify!($variant))
+            }
+
+            pub fn deserialize<'de, D>(deserializer: D) -> Result<(), D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct V;
+                impl<'de> serde::de::Visitor<'de> for V {
+                    type Value = ();
+                    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                        f.write_str(concat!("\"", stringify!($variant), "\""))
+                    }
+                    fn visit_str<E: serde::de::Error>(self, value: &str) -> Result<Self::Value, E> {
+                        if value == stringify!($variant) {
+                            Ok(())
+                        } else {
+                            Err(E::invalid_value(serde::de::Unexpected::Str(value), &self))
+                        }
+                    }
+                }
+                deserializer.deserialize_str(V)
+            }
+        }
+    };
+}
+
+mod strings {
+    named_unit_variant!(start);
+    named_unit_variant!(end);
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum ExpandProps {
     Bool(bool),
+    #[serde(with = "strings::start")]
     Start,
+    #[serde(with = "strings::end")]
     End
 }
 
